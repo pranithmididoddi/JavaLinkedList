74a01d201af6c6999cf26af3b233c14603bb30f2
pranithmididoddi <pranith13@gmail.com>
2016-12-30 11:16:49 -0600

    LinkedList cycle detection

--- src/Solution.java
+++ src/Solution.java
@@ -160,4 +160,32 @@ You only need to complete this method. */
      }
      return head;
  }
+
+ /**Linked list cycle detection*/
+
+    /**https://pranithleetcode.wordpress.com/2016/12/30/linked-list-cycle-detection-ii/*/
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast) break;
+        }
+
+        if(fast==null || fast.next==null) return null;
+
+        slow=head;
+
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        return slow;
+
+    }
 }

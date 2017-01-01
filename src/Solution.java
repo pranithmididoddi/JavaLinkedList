afc7666f738e1b71d1cd0e69d7f9362f5cf19bcd
pranithmididoddi <pranith13@gmail.com>
2017-01-01 01:54:00 -0600

    LinkedList cycle detection

--- src/Solution.java
+++ src/Solution.java
@@ -188,4 +188,46 @@ You only need to complete this method. */
         return slow;
 
     }
+    /**You are given the pointer to the head node of a linked list and you need to print all its elements in reverse order from tail to head, one element per line. The head pointer may be null meaning that the list is empty - in that case, do not print anything!
+
+     Input Format
+     You have to complete the void ReversePrint(Node* head) method which takes one argument - the head of the linked list. You should NOT read any input from stdin/console.
+
+     Output Format
+     Print the elements of the linked list in reverse order to stdout/console (using printf or cout) , one per line.
+
+     Sample Input
+
+     1 --> 2 --> NULL
+     2 --> 1 --> 4 --> 5 --> NULL
+
+     Sample Output
+
+     2
+     1
+     5
+     4
+     1
+     2*/
+
+    void ReversePrint(ListNode head) {
+       ListNode current=head;
+        ListNode previous=null;
+        ListNode nxt=null;
+
+        while(current!=null){
+            nxt=current.next;
+            current.next=previous;
+            previous=current;
+            current=nxt;
+        }
+        while(previous!=null){
+            System.out.println(previous.val);
+            previous=previous.next;
+        }
+
+    }
 }
+
+
+

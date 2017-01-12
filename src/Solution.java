8f8855b832efe8c2f1d7fc7d6440b751d158b296
pranithmididoddi <pranith13@gmail.com>
2017-01-12 12:46:33 -0600

    LinkedList cycle detection

--- src/Solution.java
+++ src/Solution.java
@@ -298,6 +298,66 @@ You only need to complete this method. */
 
         return head;
     }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = findMiddle(head);
+        ListNode tail = reverse(mid.next);
+        mid.next = null;
+
+        merge(head, tail);
+    }
+
+    public ListNode findMiddle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode current=head;
+        ListNode previous=null;
+        ListNode nxt=null;
+
+        while(current!=null){
+            nxt=current.next;
+            current.next=previous;
+            previous=current;
+            current=nxt;
+        }
+
+        return previous;
+    }
+
+    private void merge(ListNode head1, ListNode head2) {
+        int index = 0;
+        ListNode dummy = new ListNode(0);
+        while (head1 != null && head2 != null) {
+            if (index % 2 == 0) {
+                dummy.next = head1;
+                head1 = head1.next;
+            } else {
+                dummy.next = head2;
+                head2 = head2.next;
+            }
+            dummy = dummy.next;
+            index ++;
+        }
+        if (head1 != null) {
+            dummy.next = head1;
+        } else {
+            dummy.next = head2;
+        }
+    }
 }
 
 

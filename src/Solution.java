1e1448f8b4d8ddcc092b6ab0b323dba12ea9ef32
pranithmididoddi <pranith13@gmail.com>
2017-01-14 22:51:18 -0600

    odd even list

--- src/Solution.java
+++ src/Solution.java
@@ -396,6 +396,27 @@ You only need to complete this method. */
         return rightDummy.next;
     }
 
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        head=dummy;
+
+        while(head.next!=null && head.next.next!=null){
+            ListNode n1=head.next;
+            ListNode n2=head.next.next;
+
+            head.next=n2;
+            n1.next=n2.next;
+            n2.next=n1;
+
+            head=n1;
+        }
+
+        return dummy.next;
+
+    }
+
 
 }
 

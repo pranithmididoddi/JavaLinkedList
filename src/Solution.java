2ce8f53b5245c8e5c5d4f9a1ddadbeb629c6860a
Srinivas <pranith13@gmail.com>
2017-02-14 11:59:35 -0600

    addnumbers in a linkedlist

--- src/Solution.java
+++ src/Solution.java
@@ -498,6 +498,37 @@ You only need to complete this method. */
 
   /**testing if the upload is still working or not*/
 
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+      ListNode p1=l1;
+      ListNode p2=l2;
+      int carry=0;
+      ListNode dummy=new ListNode(0);
+      ListNode p3=dummy;
+
+      while(p1!=null || p2!=null){
+
+          if(p1!=null){
+              carry+=p1.val;
+              p1=p1.next;
+          }
+
+          if(p2!=null){
+              carry+=p2.val;
+              p2=p2.next;
+          }
+
+          p3.next=new ListNode(carry%10);
+          p3=p3.next;
+          carry/=10;
+
+      }
+      if(carry==1){
+          p3.next=new ListNode(1);
+      }
+      return dummy.next;
+  }
+
 }
 
 

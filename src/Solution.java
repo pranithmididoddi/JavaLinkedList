b8d7be47d26d86e6e5553dbdb84d4cae3b738bef
Srinivas <pranith13@gmail.com>
2017-03-14 12:27:02 -0500

    sort

--- src/Solution.java
+++ src/Solution.java
@@ -699,6 +699,56 @@ You only need to complete this method. */
         }
         return list.val==0?list.next:list;
     }
+
+    public ListNode sortList(ListNode head) {
+
+        if(head==null || head.next==null) return head;
+
+        ListNode mid=findMiddle(head);
+        ListNode right=sortList(mid.next);
+        mid.next=null;
+        ListNode left=sortList(head);
+
+        return Merge(left,right);
+
+    }
+
+    public ListNode mid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode Merge(ListNode l1, ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode head=dummy;
+
+        while(l1!=null || l2!=null){
+            if(l1!=null && l2!=null){
+                if(l1.val<l2.val){
+                    head.next=l1;
+                    l1=l1.next;
+                }else{
+                    head.next=l2;
+                    l2=l2.next;
+                }
+                head=head.next;
+            }else if(l1==null){
+                head.next=l2;
+                break;
+            }else if(l2==null){
+                head.next=l1;
+                break;
+            }
+        }
+        return dummy.next;
+    }
+
 }
 
 
